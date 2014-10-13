dfaffdd05b5bfbc6e64d311ac9c256cb36aaa294
xeodou <xeodou@gmail.com>
2014-10-13 14:11:48 +0800

    Export origin request.

--- index.js
+++ index.js
@@ -4,35 +4,36 @@
   http://github.com/mikeal/request
   For use in Koa.
 */
-  
+
 var _request = require('request');
 
-function request (uri, options) { 
-	return function (callback) { 
-		_request(uri, options, function (error, response, body) { 
-			callback(error, response); 
-		}) 
-	} 
+function request (uri, options) {
+    return function (callback) {
+        _request(uri, options, function (error, response, body) {
+            callback(error, response);
+        })
+    }
 }
 
 //copy request's properties
 for (var attr in _request) {
-	if (_request.hasOwnProperty(attr)) {
-		if (['get','post','put','patch','head','del'].indexOf(attr) > -1) {
-			//trunkify request's convenience methods
-			request[attr] = (function(attr) {
-				return function (uri, options) { 
-					return function (callback) { 
-						_request[attr](uri, options, function (error, response, body) { 
-							callback(error, response); 
-						}) 
-					} 
-				}
-			})(attr);
-		} else {
-			request[attr] = _request[attr];
-		}
-	}
+    if (_request.hasOwnProperty(attr)) {
+        if (['get','post','put','patch','head','del'].indexOf(attr) > -1) {
+            //trunkify request's convenience methods
+            request[attr] = (function(attr) {
+                return function (uri, options) {
+                    return function (callback) {
+                        _request[attr](uri, options, function (error, response, body) {
+                            callback(error, response);
+                        })
+                    }
+                }
+            })(attr);
+        } else {
+            request[attr] = _request[attr];
+        }
+    }
 }
 
 module.exports = request;
+module.exports = _request;
